a6528cd74ac971be48beb6e7b04d736c7b0ac1b6
Meik Kreyenkoetter <meik.kreyenkoetter@gmail.com>
2023-03-03 12:12:07 +0100

    Added earthly.

---
+++ src/earthly/install.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+set -e
+
+# Checks if packages are installed and installs them if not.
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt-get update..."
+            apt-get update -y
+        fi
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+echo "(*) Installing EARTHLY..."
+
+check_packages curl ca-certificates jq
+
+VERSION=${VERSION:-"latest"}
+echo "Requested version: $VERSION"
+
+if [ "${VERSION}" != "latest" ]; then
+    versionStr=-${VERSION}
+else
+    versionStr=$(curl https://api.github.com/repos/earthly/earthly/releases/latest | jq -r '.tag_name')
+fi
+
+archTxt="arm64"
+arch=$(uname -i)
+if [ "${arch}" = "amd64" ] || [ "${arch}" = "x86_64" ] ; then
+    archTxt="amd64"
+fi
+
+echo "Installing version: $versionStr"
+
+BIN="/usr/local/bin"
+curl -sSL \
+"https://github.com/earthly/earthly/releases/download/${versionStr}/earthly-$(uname -s | tr '[:upper:]' '[:lower:]')-${archTxt}" \
+-o "${BIN}/earthly" || exit 1
+
+chmod +x "${BIN}/earthly"
+
+echo "Done"
